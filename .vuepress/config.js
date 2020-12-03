5f19ea0c1bba14c525e065f8e550bbd06a4a372d
Ace <ace963ace@gmail.com>
2020-12-04 07:17:13 +0800

    first commit

---
+++ .vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    title: 'CheatSheet',
+    base: '/cheatsheet/',
+    description: 'cheatsheet',
+    themeConfig: {
+    },
+    markdown: {
+        anchor: { permalink: false },
+        toc: { includeLevel: [1, 2] },
+        extendMarkdown: md => {
+            // 使用更多的 markdown-it 插件!
+            // md.use(require('markdown-it-xxx'))
+        }
+    },
+    plugins: [
+        ['container', {
+            type: 'card',
+            before: info => `<div class="masonry-card"><div class="masonry-card-box"><div class="masonry-card-content"><div class="title">${info}</div>`,
+            after: '</div></div></div>',
+        }],
+    ]
+}
---
+++ .vuepress/theme/layouts/MasonryLayout.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="theme-container">
+    <Content/>
+  </div>
+</template>
+<script>
+import Masonry from "masonry-layout";
+export default {
+  mounted() {
+    new Masonry(".content__default", {
+      columnWidth: 0,
+    });
+  },
+}
+</script>
+<style>
+body{
+    background-color: #2c3e50;
+}
+.masonry-card-box{
+  padding: 0.3rem;
+}
+.masonry-card-content{
+      background-color: #2c3e50;
+    border-radius: 6px;
+    border: 1px solid #708090;
+}
+.masonry-card-content .title{
+    padding: .8rem 1rem;
+    color: #fff;
+    border-bottom: 1px solid #708090;
+}
+.masonry-card {
+  width: 100%;
+}
+/* Small (sm) */
+@media (min-width: 640px) {
+  .masonry-card {
+    width: 50%;
+  }
+}
+
+/* Medium (md) */
+@media (min-width: 768px) {
+  .masonry-card {
+    width: 50%;
+  }
+}
+
+/* Large (lg) */
+@media (min-width: 1024px) {
+  .masonry-card {
+    width: 33.33333%;
+  }
+}
+
+/* Extra Large (xl) */
+@media (min-width: 1280px) {
+  .masonry-card {
+    width: 33.33333%;
+  }
+}
+</style>
